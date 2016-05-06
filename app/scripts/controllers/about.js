80f699682a1f5b64e45a64a9e7aedbe1be5f84a3
dan <dan@commasoft.com>
2016-05-05 23:39:59 -0700

    initial check in

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phrApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the phrApp
+ */
+angular.module('phrApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phrApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the phrApp
+ */
+angular.module('phrApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
